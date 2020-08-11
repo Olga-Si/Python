5417270e843e2c6090fe00a04180120714f3ecb4
Olga-Si <68199443+Olga-Si@users.noreply.github.com>
2020-08-11 21:25:31 +0500

    Add files via upload

---
+++ Lesson_8.py
@@ -0,0 +1,129 @@
+# Task1. Реализовать класс «Дата», функция-конструктор которого должна принимать дату в виде строки формата «день-месяц-год».
+# В рамках класса реализовать два метода.
+# Первый, с декоратором @classmethod, должен извлекать число, месяц, год и преобразовывать их тип к типу «Число».
+# Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца и года (например, месяц — от 1 до 12).
+# Проверить работу полученной структуры на реальных данных.
+
+class Data:
+    def __init__(self, day_month_year):
+        self.day_month_year = str(day_month_year)
+
+    @classmethod
+    def extract(cls, day_month_year):
+        my_date = []
+
+        for i in day_month_year.split():
+            if i != '-': my_date.append(i)
+
+        return int(my_date[0]), int(my_date[1]), int(my_date[2])
+
+    @staticmethod
+    def valid(day, month, year):
+
+        if 1 <= day <= 31:
+            if 1 <= month <= 12:
+                if 2020 >= year >= 0:
+                    return f'Дата введена верно'
+                else:
+                    return f'Некорректно введен год'
+            else:
+                return f'Некорректно введен месяц'
+        else:
+            return f'Некорректно введен день'
+
+    def __str__(self):
+        return f'Текущая дата {Data.extract(self.day_month_year)}'
+
+
+today = Data('11 - 8 - 2020')
+print(today)
+print(today.valid(32, 12, 2020))
+print(Data.valid(31, 13, 2020))
+print(today.valid(31, 12, 2021))
+print(Data.extract('31 - 08 - 2020'))
+print(today.extract('31 - 08 - 2020'))
+print(Data.valid(11, 8, 2020))
+
+# Task2.Создайте собственный класс-исключение, обрабатывающий ситуацию деления на нуль.
+# Проверьте его работу на данных, вводимых пользователем.
+# При вводе пользователем нуля в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
+
+class ByNull:
+    def __init__(self, dividend, divider):
+        self.divider = divider
+        self.dividend = dividend
+
+    @staticmethod
+    def by_null(dividend, divider):
+        try:
+            return ( dividend/ divider)
+        except:
+            return (f"Делить на ноль нельзя")
+
+
+print(ByNull.by_null(10, 0))
+print(ByNull.by_null(10, 0.1))
+
+# Task3. Создайте собственный класс-исключение, который должен проверять содержимое списка на наличие только чисел.
+# Проверить работу исключения на реальном примере. Необходимо запрашивать у пользователя данные и заполнять список.
+# Класс-исключение должен контролировать типы данных элементов списка.
+# Примечание: длина списка не фиксирована. Элементы запрашиваются бесконечно, пока пользователь сам не остановит работу скрипта, введя, например, команду “stop”.
+# При этом скрипт завершается, сформированный список выводится на экран.
+# Подсказка: для данного задания примем, что пользователь может вводить только числа и строки.
+# При вводе пользователем очередного элемента необходимо реализовать проверку типа элемента и вносить его в список, только если введено число.
+# Класс-исключение должен не позволить пользователю ввести текст (не число) и отобразить соответствующее сообщение.
+# При этом работа скрипта не должна завершаться.
+
+class Error:
+    def __init__(self, *args):
+        self.my_list = []
+
+    def my_input(self):
+
+        while True:
+            try:
+                val = int(input('Введите значения и нажимайте Enter - '))
+                self.my_list.append(val)
+                print(f'Текущий список - {self.my_list} \n ')
+            except:
+                print(f"Введено некорректное значение: строка и булево")
+                start = input(f'Если хотите добавить ещё число введите start, для выхода введите stop ')
+
+                if start == 'start' or start == 'Start':
+                    print(try_except.my_input())
+                elif start == 'stop' or start == 'Stop':
+                    return f'Вы вышли'
+                else:
+                    return f'Вы вышли'
+
+try_except = Error(1)
+print(try_except.my_input())
+
+# Task7.Реализовать проект «Операции с комплексными числами».
+# Создайте класс «Комплексное число», реализуйте перегрузку методов сложения и умножения комплексных чисел.
+# Проверьте работу проекта, создав экземпляры класса (комплексные числа) и выполнив сложение и умножение созданных экземпляров.
+# Проверьте корректность полученного результата.
+
+class ComplexNumber:
+    def __init__(self, a, b, *args):
+        self.a = a
+        self.b = b
+        self.z = 'a + b * i'
+
+    def __add__(self, other):
+        print(f'Сумма z1 и z2 равна')
+        return f'z = {self.a + other.a} + {self.b + other.b} * i'
+
+    def __mul__(self, other):
+        print(f'Произведение z1 и z2 равно')
+        return f'z = {self.a * other.a - (self.b * other.b)} + {self.b * other.a} * i'
+
+    def __str__(self):
+        return f'z = {self.a} + {self.b} * i'
+
+
+z_1 = ComplexNumber(1, -2)
+z_2 = ComplexNumber(3, 4)
+print(z_1)
+print(z_1 + z_2)
+print(z_1 * z_2)
